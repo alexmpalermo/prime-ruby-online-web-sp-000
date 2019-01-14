f473e54e3a5e13082028708214a689721c719a9c
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-14 00:56:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,5 @@
 def prime?(integer)
-  array = []
-  counter = 2 
-  until counter == integer - 1
-   number = integer % counter 
-  counter += 1
-  array << number
-end
-   if array.include?(0) 
-     true
-   else 
-     false 
+
 end
 end 
 (2..integer - 1).each {|x| return false if (integer % x) == 0 }
