4f7585877ea7e08bbaea63b748a7d1bbc3735dfb
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-14 00:56:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,3 +4,14 @@ end
 end 
 (2..integer - 1).each {|x| return false if (integer % x) == 0 }
 
+  array = []
+  counter = 2 
+  until counter == integer - 1
+   number = integer % counter 
+  counter += 1
+  array << number
+end
+   if array.include?(0) 
+     true
+   else 
+     false 
